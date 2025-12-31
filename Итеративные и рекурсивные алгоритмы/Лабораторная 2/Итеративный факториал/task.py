86590a35848210de11d8bf1cc71f895d9498e883
TarantinoQuentin <gregdenfv42@gmail.com>
2025-12-31 14:28:25 +0300

    Вторая самостоятельная сделана, не смог сделать алгоритм калькулятора

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
@@ -6,14 +6,15 @@ def factorial_iterative(n: int) -> int:
     :return: n! - факториал числа n
     """
     # реализовать итеративный алгоритм нахождения факториала
-    if n < 0:
-        raise ValueError('Факториал может быть посчитан только для положительных числе')
-    elif not isinstance(n, int):
+    if not isinstance(n, int):
         raise TypeError('Факториал может быть посчитан только для натуральных чисел')
+    elif n < 0:
+        raise ValueError('Факториал может быть посчитан только для положительных числе')
+
     elif n == 0:
         return 1
 
     result = 1
-    for i in range(1, n + 1):
+    for i in range(1, n ):
         result *= i
     return result
--- Итеративные и рекурсивные алгоритмы/Самостоятельная работа/Ряды Тейлора/task.py
+++ Итеративные и рекурсивные алгоритмы/Самостоятельная работа/Ряды Тейлора/task.py
@@ -14,10 +14,29 @@ def sinx(x: Union[int, float]) -> float:
     :return: значение sin(x)
     """
     # вычислить sin(x) с помощью разложения сумму бесконечного ряда
+    #     n = 0
+    #     sinx_result = 0
+    #     while True:
+    #         step_action = pow(-1, n) * pow(x, 2 * n + 1) / factorial(2 * n + 1)
+    #         sinx_result += step_action
+    #         n += 1
+    #
+    #         if abs(step_action) <= DELTA:
+    #             return sinx_result
     n = 0
+    pow_term = -1
+    second_pow_term = x
+    factorial_term = 2 * n + 1
     sinx_result = 0
     while True:
-        step_action = pow(-1, n) * pow(x, 2 * n + 1) / factorial(2 * n + 1)
+        for _ in range(n):
+            pow_term *= pow_term
+        for _ in range(2 * n + 1):
+            second_pow_term *= second_pow_term
+        for i in range(1, factorial_term):
+            factorial_term *= i
+
+        step_action = pow_term * second_pow_term / factorial_term
         sinx_result += step_action
         n += 1
 
