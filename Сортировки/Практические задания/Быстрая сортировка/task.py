a11868d2c2df92ecf37b40a13aaca86573a645ed
TarantinoQuentin <gregdenfv42@gmail.com>
2026-01-16 22:29:57 +0300

    Сделал третью практику

--- Сортировки/Практические задания/Быстрая сортировка/task.py
+++ Сортировки/Практические задания/Быстрая сортировка/task.py
@@ -1,6 +1,30 @@
 from typing import List
 
 
+def partition(arr: list, low: int, high: int):
+    pivot = arr[high]
+
+    left_edge_index = low - 1
+
+    for checking_index in range(low, high):
+        if arr[checking_index] <= pivot:
+            left_edge_index += 1
+            arr[left_edge_index], arr[checking_index] = arr[checking_index], arr[left_edge_index]
+
+    arr[left_edge_index + 1], arr[high] = arr[high], arr[left_edge_index + 1]
+
+    return left_edge_index + 1
+
+def quick_sort(arr: list, low: int = 0, high: int | None = None):
+    if high is None:
+        high = len(arr) - 1
+
+    if low < high:
+        pivot = partition(arr, low, high)
+
+        quick_sort(arr, low, pivot - 1)
+        quick_sort(arr, pivot + 1, high)
+
 def sort(container: List[int]) -> List[int]:
     """
     Алгоритм быстрой сортировки.
@@ -12,4 +36,28 @@ def sort(container: List[int]) -> List[int]:
     :param container: последовательность, которую надо отсортировать
     :return: Отсортированная в порядке возрастания последовательность
     """
-    ...  # TODO реализовать алгоритм быстрой сортировки
+    # реализовать алгоритм быстрой сортировки
+
+    if not container:
+        return container
+
+    quick_sort(container)
+
+    return container
+
+# Решение создателя теста:
+
+#     if not container:
+#         return container
+#
+#     pivot = container[0]
+#     return (
+#             sort([item for item in container if item < pivot]) +
+#             [item for item in container if item == pivot] +
+#             sort([item for item in container if item > pivot])
+#     )
+
+# Чем оно плохо? Алгоритм быстрой сортировки в классическом виде хорош тем,
+# что работает "in place", не затрачивая дополнительную память на копирование кусков
+# массива, но в данном случае через list comprehension мы его дублируем аж три раза,
+# неоправданно затрачивая на это лишнюю память.
--- Сортировки/Практические задания/Сортировка слиянием/task.py
+++ Сортировки/Практические задания/Сортировка слиянием/task.py
@@ -12,4 +12,40 @@ def sort(container: List[int]) -> List[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализуйте сортировку слиянием
+    # реализуйте сортировку слиянием
+    def merge(left: list[int], right: list[int]) -> list[int]:
+        """
+        Алгоритм для слияния левой и правой части, сравнивает
+        первые элементы из каждого массива и в итоговый массив записывает наименьшее,
+        в массиве, в котором был наименьший элемент, переходит к следующему,
+        когда один из массивов закончится, остаток второго «сливает» в итоговый массив
+
+        :param left: левая часть
+        :param right: правая часть
+        :return: слитый список
+        """
+        result = []
+        while True:
+            if left[0] <= right[0]:
+                result.append(left.pop(0))
+            else:
+                result.append(right.pop(0))
+            if not left:
+                result.extend(right)
+                break
+            if not right:
+                result.extend(left)
+                break
+        return result
+
+    if len(container) < 2:
+        return  container
+
+    middle_index = len(container) // 2
+    left_part = container[:middle_index]
+    right_part = container[middle_index:]
+
+    sort_left_container = sort(left_part)
+    sort_right_container = sort(right_part)
+
+    return merge(sort_left_container, sort_right_container)
