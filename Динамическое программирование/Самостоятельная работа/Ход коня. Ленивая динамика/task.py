f6b9667dab1ee5af6ff0e3bf4966a27cbaa808b2
TarantinoQuentin <gregdenfv42@gmail.com>
2026-01-27 21:50:27 +0300

    Начал четвертую самостоятельную

--- Динамическое программирование/Самостоятельная работа/Ход коня. Ленивая динамика/task.py
+++ Динамическое программирование/Самостоятельная работа/Ход коня. Ленивая динамика/task.py
@@ -9,9 +9,32 @@ def calculate_paths(shape: Tuple[int, int]) -> int:
     :param shape: размер доски в виде кортежа
     :return: количество путей согласно заданным условиям
     """
-    ... # TODO реализуйте подсчет ходов коня
+    # реализуйте подсчет ходов коня
+
+    # Решить не смог, решение учителя:
+    rows, cols = shape  # Кол-во строк и столбцов отсчет с 1
+
+    @lru_cache  # @lru_cache(maxsize=None) == lru_cache
+    def get_steps(i, j):
+        if i == 0 and j == 0:  # Левый верхний угол
+            return 1
+
+        if not 0 <= i < rows:  # Выпадаю за границы поля для строк
+            return 0
+
+        if not 0 <= j < cols:  # Выпадаю за границы поля для столбцов
+            return 0
+
+        return sum([
+            get_steps(i - 2, j + 1),
+            get_steps(i - 2, j - 1),
+            get_steps(i - 1, j - 2),
+            get_steps(i + 1, j - 2),
+        ])
+
+    return get_steps(rows-1, cols-1)
 
 
 if __name__ == '__main__':
     print(calculate_paths((4, 4)))  # 2
-    print(calculate_paths((7, 15)))  # 13309
+    # print(calculate_paths((7, 15)))  # 13309
